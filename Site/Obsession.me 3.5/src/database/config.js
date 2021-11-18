be26ce3b9f4968e4ec623f9d8786611c638a9eeb
Pedro Tresmondi <pedro.tresmondi@bandtec.com.br>
2021-11-17 23:32:44 -0300

    3.5.0

--- Site/Obsession.me 3.5/src/database/config.js
+++ Site/Obsession.me 3.5/src/database/config.js
@@ -20,7 +20,7 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "root",
+    user: "dev",
     database: "obsession",
     password: "1234567",
 };
